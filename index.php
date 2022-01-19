dc8f40b50f81ac5942ed847dda9bd35e8c85a71b
Giovanni Victorio <56368148+GiovanniVictorio@users.noreply.github.com>
2022-01-18 23:10:20 -0700

    Menu Programming

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Poké Extra</title>
+
+    <!-- Links CSS -->
+    <link rel="stylesheet" href="assets/css/styles.css">
+
+    <!-- Boxicon -->
+    <link href='https://unpkg.com/boxicons@2.1.1/css/boxicons.min.css' rel='stylesheet'>
+</head>
+<body>
+    <!-- Codigo del menu -->
+    <?php include 'assets/html/menu.html' ?>
+</body>
+</html>
